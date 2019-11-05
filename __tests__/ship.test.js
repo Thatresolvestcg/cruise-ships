615aa81b8aaa7f27547445fb0c8e86fd6c4dfb41
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-05 20:41:42 +0000

    added a way to change state of startingPort, will likely update this to currentPort added tests to check we can both set a specific port

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -1,11 +1,23 @@
-const Ship = require("../src/ship");
+const Ship = require('../src/ship');
 
-describe('ship', () => {
-    test("New ship can be created and it is an array", () => {
+describe('Ship', () => {
+    test('New ship can be created and it is an array', () => {
         expect(new Ship()).toBeInstanceOf(Object);
     });
-    test("the ship has a starting point", () => {
-        const ship = new Ship("Dover");
-        expect(ship.startingPort).toBe("Dover");
+    test('the ship has a starting point', () => {
+        const ship = new Ship('Dover');
+        expect(ship.startingPort).toBe('Dover');
+    });
+});
+
+describe('Sail', () => {
+    const ship = new Ship('Dover');
+    test('Ship can setSail and update startingPort', () => {
+        ship.setSail();
+        expect(ship.startingPort).toBeFalsy();
+    });
+    test("Ship can sail to a specific port", () => {
+        ship.setSail('Barry Island');
+        expect(ship.startingPort).toEqual('Barry Island');
     });
 });
--- src/ship.js
+++ src/ship.js
@@ -1,12 +1,9 @@
-// As a cruise ship captain,
-// So I can get passengers aboard a ship,
-// I want a ship to have a starting port.
-
-// ship = object 
-// startingPort = property
-
 function Ship(startingPort) {
-    this.startingPort = "startingPort";
-}
+    this.startingPort = startingPort;
+
+    Ship.prototype.setSail = function (newPort) {
+        this.startingPort = newPort
+    };
+};
 
 module.exports = Ship;
