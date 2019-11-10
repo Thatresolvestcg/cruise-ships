5a285c5b5a08658ba00f33392278412dbbebf5f7
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-10 17:29:01 +0000

    dock func now passes testing

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -7,15 +7,28 @@ describe('Ship', () => {
         expect(new Ship()).toBeInstanceOf(Object);
     });
     test('has a starting point', () => {
-        const ship = new Ship('Dover');
-
-        expect(ship.startingPort).toBe('Dover');
+        const port = new Port('Dover')
+        const ship = new Ship(port);
+        expect(ship.currentPort).toBe(port);
     });
     test('can set sail', () => {
-        const ship = new Ship('Dover');
+        const port = new Port('Dover');
+        const ship = new Ship(port);
 
         ship.setSail();
 
-        expect(ship.startingPort).toBeFalsy();
+        expect(ship.currentPort).toBeFalsy();
+    });
+});
+
+describe('Dock', () => {
+    test('can dock at a different port', () => {
+        const dover = new Port('Dover');
+        const ship = new Ship(dover)
+
+        const calais = new Port('Calais');
+        ship.dock(calais);
+
+        expect(ship.currentPort).toBe(calais);
     });
 });
