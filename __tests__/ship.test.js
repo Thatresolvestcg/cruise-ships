8ba654e79c4407fae6cfb139d999bc245891f905
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-14 11:35:21 +0000

    used beforeEach() to make the ship tests dry

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -4,37 +4,43 @@ const Port = require('../src/Port.js');
 const Itinerary = require('../src/Itinerary.js')
 
 describe('Ship', () => {
-    test('can be instantiated', () => {
-        const port = new Port('Knowhere');
-        const itinerary = new Itinerary([port]);
-        const ship = new Ship(itinerary);
+    describe('with ports and an itiniary', () => {
+        let ship;
+        let knowhere;
+        let wakanda;
+        let itinerary;
+        beforeEach(() => {
+            wakanda = new Port('Wakanda');
+            knowhere = new Port('Knowhere');
+            itinerary = new Itinerary([wakanda, knowhere]);
+            ship = new Ship(itinerary);
+        });
+        test('can be instantiated', () => {
+            expect(ship).toBeInstanceOf(Object);
+        });
+        test('has a starting point', () => {
+            const port = new Port('Wakanda');
+            const itinerary = new Itinerary([port])
+            const ship = new Ship(itinerary);
+            expect(ship.currentPort).toBe(port);
+        });
+        test('can set sail', () => {
 
-        expect(ship).toBeInstanceOf(Object);
-    });
-    test('gets added to port on instantiation', () => {
-        const knowhere = new Port('Knowhere');
-        const itinerary = new Itinerary([knowhere]);
-        const ship = new Ship(itinerary);
+            const itinerary = new Itinerary([knowhere, wakanda]);
+            const ship = new Ship(itinerary);
 
-        expect(knowhere.ships).toContain(ship);
-    });
-    test('has a starting point', () => {
-        const port = new Port('Wakanda');
-        const itinerary = new Itinerary([port])
-        const ship = new Ship(itinerary);
-        expect(ship.currentPort).toBe(port);
-    });
-    test('can set sail', () => {
-        const knowhere = new Port('Knowhere');
-        const wakanda = new Port('Wakanda');
-        const itinerary = new Itinerary([knowhere, wakanda]);
-        const ship = new Ship(itinerary);
+            ship.setSail();
 
-        ship.setSail();
+            expect(ship.currentPort).toBeFalsy();
+            expect(knowhere.ships).not.toContain(ship);
+        });
+        test('gets added to port on instantiation', () => {
+            const itinerary = new Itinerary([knowhere]);
+            const ship = new Ship(itinerary);
 
-        expect(ship.currentPort).toBeFalsy();
-        expect(knowhere.ships).not.toContain(ship);
-    });
+            expect(knowhere.ships).toContain(ship);
+        });
+    })
     test('cannot sail further than its itinerary', () => {
         const knowhere = new Port('Knowhere');
         const wakanda = new Port('Wakanda');
