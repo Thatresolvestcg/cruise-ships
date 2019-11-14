ce42d02b314017cf977052c741b4b042883b5747
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-14 11:47:06 +0000

    made port dry

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -3,32 +3,36 @@ const Port = require('../src/Port.js');
 const Ship = require('../src/Ship.js')
 
 describe('Port', () => {
-    test('can be instantiated', () => {
-        expect(new Port()).toBeInstanceOf(Object);
-    });
-    test('can add a ship', () => {
-        const port = new Port('Knowhere');
-        const ship = {};
+    describe('with ports and ships', () => {
+        let port;
+        let ship;
+        let milano;
+        let quinJet;
+        beforeEach(() => {
+            port = new Port('Knowhere');
+            ship = {}
+            milano = {}
+            quinJet = {}
+        });
 
-        port.addShip(ship);
+        test('can be instantiated', () => {
+            expect(new Port()).toBeInstanceOf(Object);
+        });
+        test('can add a ship', () => {
+            port.addShip(ship);
 
-        expect(port.ships).toContain(ship);
-    });
-    test('can remove a ship', () => {
-        const port = new Port('Knowhere');
-        const milano = {};
-        const quinJet = {};
+            expect(port.ships).toContain(ship);
+        });
+        test('can remove a ship', () => {
+            port.addShip(milano);
+            port.addShip(quinJet);
+            port.removeShip(quinJet);
 
-        port.addShip(milano);
-        port.addShip(quinJet);
-        port.removeShip(quinJet);
+            expect(port.ships).toEqual([milano])
+        });
+        test('throws an error if entry does not exist', () => {
+            expect(() => port.removeShip(quinJet)).toThrowError('This entry does not exist');
 
-        expect(port.ships).toEqual([milano])
-    });
-    test('throws an error if entry does not exist', () => {
-        const port = new Port('Knowhere');
-        const quinJet = {};
-        expect(() => port.removeShip(quinJet)).toThrowError('This entry does not exist');
-
+        });
     });
 }); 
