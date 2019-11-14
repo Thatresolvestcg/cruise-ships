a58764b357e3234638b5bac2e719c4b28e23cc98
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-14 09:46:19 +0000

    tidied up the removeShip throwing an error if entry doesnt exist

--- src/port.js
+++ src/port.js
@@ -10,12 +10,16 @@ class Port {
     }
     removeShip(ship) {
         const remove = this.ships.indexOf(ship);
-        const ships = this.ships;
-        ships.splice(remove, 1);
+        if (remove == -1) {
+            throw new Error('This entry does not exist')
+        } else { this.ships.splice(remove, 1); }
 
     }
 };
 
+// if (currentPortIndex === (itinerary.ports.length - 1)) {
+//     throw new Error('End of itinerary reached.');
+// }
 
 
 
